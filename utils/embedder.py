6b238c58bba73f77325429b8566cca4b83d077d1
aashi kashyap <aashikashyap@aashis-MacBook-Pro.local>
2026-05-11 08:59:37 +0530

    Removed API key from repo

--- utils/embedder.py
+++ utils/embedder.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_embedding(text: str) -> list:
